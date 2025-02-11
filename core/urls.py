4ff57efcf1bd3564d1455b5812799c31ae59a0e0
marcelopedroso <md.pedroso@hotmail.com>
2025-02-10 21:35:11 -0300

    up health check

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("health/db/", include("watchman.urls")),
+    path("health/services/", include("health_check.urls")),  # Health Check para cache e storage
 ]
